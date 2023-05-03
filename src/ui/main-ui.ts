1111b91d144c4200efa0756b3826bab6ef226e73
mrmagic2020 <2732298611@qq.com>
2023-05-03 20:28:48 +1000

    new: main ui functions

---
+++ src/ui/main-ui.ts
@@ -0,0 +1,33 @@
+import {
+  window, checkbox, WindowTemplate, label, compute
+} from "openrct2-flexui";
+
+import {
+  dataStructure
+} from "../data/data"
+
+function onClickMenuItem()
+{
+  const win_desc : WindowTemplate = window(
+    {
+      title: "Remote Handymen",
+      width: 300,
+      height: 100,
+      content: [
+        checkbox(
+          {
+            text: "Enable Remote Handymen",
+            tooltip: "Main switch for the plugin.",
+            isChecked: dataStructure.enabled.store.get(),
+            onChange: (is) => {
+              dataStructure.enabled.store.set(is);
+            }
+          }
+        )
+      ]
+    }
+  );
+  win_desc.open();
+}
+
+export { onClickMenuItem };
