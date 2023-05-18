b129db11d9d9a4da840a69f86a1a481f3838575d
mrmagic2020 <2732298611@qq.com>
2023-05-18 16:47:26 +1000

    add: handyman clear issue limit

--- src/function/function.ts
+++ src/function/function.ts
@@ -1,6 +1,19 @@
 var subscription : IDisposable;
 var subscribed : boolean = false;
+var issueLimit : number;
 
+/**
+ * Set the limit of path issues a handyman can remotely clear
+ * @param limit 
+ */
+export function setIssueLimit(limit : number) {
+  issueLimit = limit;
+}
+
+/**
+ * Register remote path cleaning
+ * @param toggle register if true, otherwise unsubscribe
+ */
 export function regCleanPathAction(toggle : boolean) {
   if (toggle && !subscribed) { // enable function
     subscribed = true;
@@ -9,10 +22,14 @@ export function regCleanPathAction(toggle : boolean) {
       handymen.forEach((handyman, i) => { // iterate through handymen list
         const patrolArea = handyman.patrolArea;
         const issues = map.getAllEntities("litter");
+        let counter : number = 0;
         issues.forEach(issue => { // check if litter entity is within the patrol area of this handyman
-          if (patrolArea.contains({x: issue.x, y: issue.y})) {
+          if (counter <= issueLimit && patrolArea.contains({x: issue.x, y: issue.y})) {
             issue.remove(); // remove the litter
           }
+          counter ++;
+          if (counter > issueLimit)
+            console.log(`Above issue limit (${counter} > ${issueLimit})`);
         });
         console.log(`Handyman ${i} Orders: ` + handyman.orders.toString());
       });
